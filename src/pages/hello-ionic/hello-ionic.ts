e4dc8e25143c46299381bebecc3440a618c2c2ae
LAPTOP-1I8ANLT0\Kevin <sam@norbrik.com>
2018-10-05 17:55:08 +0545

    first message

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component,NgZone } from '@angular/core';
+import {Facebook,FacebookLoginResponse} from '@ionic-native/facebook';
 
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-  constructor() {
+  userData=null;
+  constructor(private facebook:Facebook,private _ngZone: NgZone) {
 
+  }
+  loginWithFB(){
+    this.userData = '';
+    this.facebook.login(['email','public_profile','user_photos','user_friends']).then((response:FacebookLoginResponse)=>{
+      
+   //this.facebook.api('/me?fields=name,gender,birthday,email,picture', [])
+   this.facebook.api('me?fields=id,name,email,picture.width(720).height(720).as(picture_large)',[])
+      .then((profile)=>{
+        this._ngZone.run(() => {this.userData = profile; });
+          
+       // this.userData = {email:profile['email'],first_name:profile['first_name'],picture:profile['picture_large']['data']['url'],username:profile['name']};
+      });
+    });
+      
   }
 }
